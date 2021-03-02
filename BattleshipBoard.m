68645815aee3ead035e414420eef68bc4e18ccbb
desmondg19 <73854789+desmondg19@users.noreply.github.com>
2021-03-01 21:28:54 -0500

    Updates to GUI, added three buttons to make different levels, and testing how to highlight specific pixels

--- BattleshipBoard.m
+++ BattleshipBoard.m
@@ -22,7 +22,7 @@ function varargout = BattleshipBoard(varargin)
 
 % Edit the above text to modify the response to help BattleshipBoard
 
-% Last Modified by GUIDE v2.5 01-Mar-2021 12:53:32
+% Last Modified by GUIDE v2.5 01-Mar-2021 21:24:07
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -43,7 +43,7 @@ else
 end
 % End initialization code - DO NOT EDIT
 
-
+end
 % --- Executes just before BattleshipBoard is made visible.
 function BattleshipBoard_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
@@ -54,10 +54,10 @@ function BattleshipBoard_OpeningFcn(hObject, eventdata, handles, varargin)
 
 % Choose default command line output for BattleshipBoard
 handles.output = hObject;
-
+handles.gamestate=zeros(100);
 % Update handles structure
 guidata(hObject, handles);
-
+end
 % UIWAIT makes BattleshipBoard wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
@@ -71,3 +71,40 @@ function varargout = BattleshipBoard_OutputFcn(hObject, eventdata, handles)
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
+end
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+disp('Level: easy');
+
+end
+
+% --- If Enable == 'on', executes on mouse press in 5 pixel border.
+% --- Otherwise, executes on mouse press in 5 pixel border or over pushbutton1.
+function pushbutton1_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+end
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+disp('Level: medium');
+end
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+disp('Level: hard');
+end
