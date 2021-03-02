0f788a048958dd1569ec22d4886402cc230a77bd
mayamundada <79725445+mayamundada@users.noreply.github.com>
2021-03-02 07:47:10 -0800

    Update playershipplacer.m

--- playershipplacer.m
+++ playershipplacer.m
@@ -33,8 +33,6 @@ if direction == V && ypos + size >= 10
 end
 ypos = round(ypos);
 
-
-
 if xpos < 1 | xpos > 10
     disp('Invalid X, try again');
     xpos = input('What x coordinate do you want to hit (1-10)?');
