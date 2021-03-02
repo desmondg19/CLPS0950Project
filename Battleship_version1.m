4d91dee718e57e110cb7ee4bd02bebb87543a355
mayamundada <mayamundada@mayas-mbp.lan>
2021-03-02 07:58:03 -0800

    copied sam's code into this doc to try to resolve pull confilcts

--- Battleship_version1.m
+++ Battleship_version1.m
@@ -8,7 +8,11 @@ function []= Battleship ();
 
 disp('Welcome to Battleship');
 difficulty = input ('Choose your difficulty: 1=easy, 2=medium, 3=hard');
-
+if difficulty ~= 1 || difficulty ~= 2 || difficulty ~= 3
+    disp('This is not a valid difficulty. Please try again.');
+    difficulty = input ('Choose your difficulty: 1=easy, 2=medium, 3=hard');
+end    
+   
 %initializing the ships and the ship lengths
 shiplist = [char("C"), char("B"), char("R"), char("S"), char("D")];
 shiplength = [5, 4, 3, 3, 2];
@@ -17,6 +21,8 @@ shiplength = [5, 4, 3, 3, 2];
 [X,Y]=meshgrid(0:10);
 mesh(X,Y)
 view(2)
+ 
+
 
 %% One player version of the game
 % the code within this next for loop allows the computer to randomly
@@ -26,7 +32,7 @@ view(2)
 %   that function. that function can also be used for two player versions.
 
 board = shipplacer(shiplist, shiplength);
-
+playerboard = playershipplacer(xpos, ypos, direction);
 %this is where the game actually starts. 
 %the user is prompted to enter a number of guesses. 
 
